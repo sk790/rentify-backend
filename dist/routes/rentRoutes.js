4f73df194e6a09bf10778e92dd822230b8ccec24
sk790 <saurabhk2890@gmail.com>
2025-01-30 20:54:13 +0530

    trying to fix server less functon error on vercel

--- dist/routes/rentRoutes.js
+++
@@ -1,7 +0,0 @@
-import expres from "express";
-import { verify } from "../middlewares/verify.js";
-import { getUserRentedProducts, toggleRent, } from "../controllers/rent.controller.js";
-const router = expres.Router();
-router.route("/toggle/:productId").put(verify, toggleRent);
-router.route("/get-rented").get(verify, getUserRentedProducts);
-export default router;
