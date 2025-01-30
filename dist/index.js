af15e4173cae6ed80b06e613123fc49bb489585d
sk790 <saurabhk2890@gmail.com>
2025-01-30 20:18:09 +0530

    trying to fix server less functon error on vercel

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,8 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 import express from "express";
 import { connectToDB } from "./config/db.js";
 import { configDotenv } from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { Server } from "socket.io";
-import { Message } from "./models/chat.Model.js";
 configDotenv();
 import authRouter from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
@@ -31,49 +20,47 @@ app.use(cors({
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRoutes);
 app.use("/api/chat", chatRoutes);
-const expressServer = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
-export const io = new Server(expressServer, {
-    cors: {
-        origin: "*", // Adjust this to match your front-end origin
-    },
-});
+// export const io = new Server(expressServer, {
+//   cors: {
+//     origin: "*", // Adjust this to match your front-end origin
+//   },
+// });
 const users = {}; // Store userId -> socketId mapping
 export const getReceiverSocketId = (receiverId) => {
     return users[receiverId];
 };
-io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
-    const userId = socket.handshake.query.userId;
-    if (userId && userId !== "undefined") {
-        users[userId] = socket.id;
-        console.log(`User registered: ${userId} -> ${socket.id}`);
-    }
-    io.emit("getOnlineUsers", Object.keys(users));
-    socket.on("sendMessage", (_a) => __awaiter(void 0, [_a], void 0, function* ({ senderId, receiverId, message }) {
-        try {
-            if (!message || !senderId || !receiverId)
-                return;
-            const newMessage = new Message({
-                sender: senderId,
-                receiver: receiverId,
-                text: message,
-            });
-            yield newMessage.save();
-            const receiverSocketId = getReceiverSocketId(receiverId);
-            if (receiverSocketId) {
-                io.to(receiverSocketId).emit("newMessage", newMessage);
-                console.log(`Sent newMessage event to ${receiverSocketId}`);
-            }
-        }
-        catch (error) {
-            console.error("Error saving message:", error);
-        }
-    }));
-    socket.on("disconnect", () => {
-        console.log(`User disconnected: ${userId}`);
-        delete users[userId];
-        io.emit("getOnlineUsers", Object.keys(users));
-    });
-});
+// io.on("connection", (socket) => {
+//   console.log("A user connected:", socket.id);
+//   const userId = socket.handshake.query.userId as string;
+//   if (userId && userId !== "undefined") {
+//     users[userId] = socket.id;
+//     console.log(`User registered: ${userId} -> ${socket.id}`);
+//   }
+//   io.emit("getOnlineUsers", Object.keys(users));
+//   socket.on("sendMessage", async ({ senderId, receiverId, message }) => {
+//     try {
+//       if (!message || !senderId || !receiverId) return;
+//       const newMessage = new Message({
+//         sender: senderId,
+//         receiver: receiverId,
+//         text: message,
+//       });
+//       await newMessage.save();
+//       const receiverSocketId = getReceiverSocketId(receiverId);
+//       if (receiverSocketId) {
+//         io.to(receiverSocketId).emit("newMessage", newMessage);
+//         console.log(`Sent newMessage event to ${receiverSocketId}`);
+//       }
+//     } catch (error) {
+//       console.error("Error saving message:", error);
+//     }
+//   });
+//   socket.on("disconnect", () => {
+//     console.log(`User disconnected: ${userId}`);
+//     delete users[userId];
+//     io.emit("getOnlineUsers", Object.keys(users));
+//   });
+// });
--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import http from "http";
 import { Server } from "socket.io";
-import { Message } from "./models/chat.Model.js";
 configDotenv();
 
 import authRouter from "./routes/authRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 const port = process.env.PORT || 5000;
-
 const app = express();
+
 // Middleware
 connectToDB();
 app.use(express.json());
@@ -28,15 +27,15 @@ app.use("/api/auth", authRouter);
 app.use("/api/product", productRoutes);
 app.use("/api/chat", chatRoutes);
 
-const expressServer = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
 
-export const io = new Server(expressServer, {
-  cors: {
-    origin: "*", // Adjust this to match your front-end origin
-  },
-});
+// export const io = new Server(expressServer, {
+//   cors: {
+//     origin: "*", // Adjust this to match your front-end origin
+//   },
+// });
 
 const users: Record<string, string> = {}; // Store userId -> socketId mapping
 
@@ -44,40 +43,40 @@ export const getReceiverSocketId = (receiverId: string) => {
   return users[receiverId];
 };
 
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+// io.on("connection", (socket) => {
+//   console.log("A user connected:", socket.id);
 
-  const userId = socket.handshake.query.userId as string;
-  if (userId && userId !== "undefined") {
-    users[userId] = socket.id;
-    console.log(`User registered: ${userId} -> ${socket.id}`);
-  }
+//   const userId = socket.handshake.query.userId as string;
+//   if (userId && userId !== "undefined") {
+//     users[userId] = socket.id;
+//     console.log(`User registered: ${userId} -> ${socket.id}`);
+//   }
 
-  io.emit("getOnlineUsers", Object.keys(users));
+//   io.emit("getOnlineUsers", Object.keys(users));
 
-  socket.on("sendMessage", async ({ senderId, receiverId, message }) => {
-    try {
-      if (!message || !senderId || !receiverId) return;
-      const newMessage = new Message({
-        sender: senderId,
-        receiver: receiverId,
-        text: message,
-      });
-      await newMessage.save();
+//   socket.on("sendMessage", async ({ senderId, receiverId, message }) => {
+//     try {
+//       if (!message || !senderId || !receiverId) return;
+//       const newMessage = new Message({
+//         sender: senderId,
+//         receiver: receiverId,
+//         text: message,
+//       });
+//       await newMessage.save();
 
-      const receiverSocketId = getReceiverSocketId(receiverId);
-      if (receiverSocketId) {
-        io.to(receiverSocketId).emit("newMessage", newMessage);
-        console.log(`Sent newMessage event to ${receiverSocketId}`);
-      }
-    } catch (error) {
-      console.error("Error saving message:", error);
-    }
-  });
+//       const receiverSocketId = getReceiverSocketId(receiverId);
+//       if (receiverSocketId) {
+//         io.to(receiverSocketId).emit("newMessage", newMessage);
+//         console.log(`Sent newMessage event to ${receiverSocketId}`);
+//       }
+//     } catch (error) {
+//       console.error("Error saving message:", error);
+//     }
+//   });
 
-  socket.on("disconnect", () => {
-    console.log(`User disconnected: ${userId}`);
-    delete users[userId];
-    io.emit("getOnlineUsers", Object.keys(users));
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log(`User disconnected: ${userId}`);
+//     delete users[userId];
+//     io.emit("getOnlineUsers", Object.keys(users));
+//   });
+// });
